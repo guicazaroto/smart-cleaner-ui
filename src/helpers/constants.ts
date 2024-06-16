784693a4d3140225be1cd0431d28156f53d32a11
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-16 17:08:46 -0300

    add env var

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://production-api.eba-ecm2h3cp.us-east-1.elasticbeanstalk.com/api/v1'
+const temp = 'http://production-api.eba-ecm2h3cp.us-east-1.elasticbeanstalk.com/api/v1'
+
+export const BASE_URL = process.env.BASE_URL || temp
