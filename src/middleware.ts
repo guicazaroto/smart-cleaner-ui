360576b50a4ee2ff12e53c87b68cb2cd765cec58
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-16 22:31:27 -0300

    remove unused

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import type { NextRequest, NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
