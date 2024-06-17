41a5ab026c35080b1995d62af5073982e30ec301
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-16 22:28:38 -0300

    save

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {  useEffect } from "react";
+import {  useEffect, useState } from "react";
 import "../globals.css";
 import Head from "./head";
 import { BASE_URL } from "@/helpers/constants";
@@ -12,11 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const router = useRouter();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     async function getMe() {
       const token = Cookies.get("token");
-      
+
       if (!token) {
         router.push("/entrar");
         return;
@@ -29,11 +30,13 @@ export default function RootLayout({
       });
 
       if (!response.ok) {
+        Cookies.remove("token");
         router.push("/entrar");
       }
 
       const user = await response.json();
       
+      setUser(user);
       sessionStorage.setItem("user", JSON.stringify(user));
     }
 
@@ -45,7 +48,17 @@ export default function RootLayout({
     <html lang="en">
       <body>
        <Head />
-        <main>{children}</main>
+       {user ? (
+         <main>{children}</main>
+       ) : (
+        <div className="flex items-center justify-center h-screen">
+          <div className="relative">
+            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+            <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+            </div>
+          </div>
+        </div>
+       )}
       </body>
     </html>
   );
