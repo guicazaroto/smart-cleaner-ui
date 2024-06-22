d454475e6e06058a6a04fb7b863f38294300303f
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-21 21:54:23 -0300

    add temporary constant

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,2 @@
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://production-api.eba-ecm2h3cp.us-east-1.elasticbeanstalk.com/api/v1'
