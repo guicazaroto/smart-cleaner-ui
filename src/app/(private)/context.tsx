a9b3b85605d237636b761f99ce9bd4e53ed5c089
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-21 22:37:45 -0300

    rename

--- src/app/(private)/context.tsx
+++ src/app/(private)/context.tsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { User } from "../app/(public)/cadastro/helpers/types";
+import { User } from "../(public)/cadastro/helpers/types";
 
 export const UserContext = createContext({} as User);
