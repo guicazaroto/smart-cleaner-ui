c3c9c93b1f8a3e69694096f53deb9336bb8db0ab
Rafael Lima <santisraeusou@gmail.com>
2024-06-27 18:22:55 -0300

    refactor

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,3 @@
 
-export const BASE_URL = process.env.API_URL || 'http://production-api.eba-ecm2h3cp.us-east-1.elasticbeanstalk.com/api/v1'
+export const BASE_URL = process.env.API_URL
 export const DEFAULT_TOKEN = process.env.DEFAULT_TOKEN || ''
