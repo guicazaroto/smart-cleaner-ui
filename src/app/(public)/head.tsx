a00629ce6352b984b94046b3913739f2cb8c9051
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-06-29 22:08:10 -0300

    deploy

--- src/app/(public)/head.tsx
+++ src/app/(public)/head.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="bg-white shadow-md">
-      <div className="container mx-auto flex justify-between items-center py-4 px-8">
+      <div className="h-20 container mx-auto flex justify-between items-center py-4 px-8">
         <div className="flex items-center">
           <Link href="/">
             <span className="font-bold text-xl text-blue-500">SmartCleaner</span>
